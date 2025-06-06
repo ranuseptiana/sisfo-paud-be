9c80bf745d9d996ca145c477e45e8317f77448c7
ranuseptiana <ranugibran05@gmail.com>
2025-06-06 21:46:07 +0700

    banyak perubahan banyak eror

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['https://sistem-informasi-paud.vercel.app'],
     'allow_credentials' => true,
     'supports_credentials' => true,
 ];
